6a2c4e27c523acb6b2739c38b495776aa7561794
MOalajam <68443168+MOalajam@users.noreply.github.com>
2020-09-07 23:42:44 -0700

    Update dna.py

--- dna.py
+++ dna.py
@@ -1,43 +1,47 @@
 from sys import argv, exit
 import csv
 
-#TODO
+
 def str_count ( dna , Str ):
     count = 0
 
     for letter in range(len(dna)):
         is_str = True
+        if letter < len(dna) - len(Str):
+            for i in range(len(Str)) :
 
-        for i in range(len(Str)) :
+                if dna[letter + i] != Str[i] :
+                    is_str = False
 
-            if dna[letter + i] != Str[i] :
-                is_str = False
+            if is_str == True:
+                count += 1
 
-        if is_str == True:
-            count += 1
-    return count
+    print(count)
+
 
 
 with open(argv[1] , "r") as data_file :
     reader = csv.reader(data_file)
     data = list(reader)
 
+
 with open(argv[2] , "r") as dna_file :
     in_dna = dna_file.read()
+
 
 
 DNA_str_info_list = []
 
 for  i in range(1 , len(data)) :
-    DNA_str_info_list.append(str_count(in_dna,data[0][i]))
-    
-for j in range (1 , len(data[0]))    
+    DNA_str_info_list.append(str_count(in_dna,data[i][0]))
+
+for j in range (1 , len(data[0]))
     for p in range(len(data))
-                 
+
         person_found = True
-        if(data[p + 1][j] != DNA_str_info_list[p]):
-            person_found = False          
-         
+        if(data[j][p + 1] != DNA_str_info_list[p]):
+            person_found = False
+
     if person_found == True :
         print(data[0][j])
         exit(0)
