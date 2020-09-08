d1a655c11d4314d613ac4dadf59217f698373ad5
MOalajam <68443168+MOalajam@users.noreply.github.com>
2020-09-08 08:49:29 -0700

    Update dna.py

--- dna.py
+++ dna.py
@@ -1,47 +1,68 @@
 from sys import argv, exit
 import csv
 
-
 def str_count ( dna , Str ):
     count = 0
+    max_count = 0
+    k = 0
 
-    for letter in range(len(dna)):
+    for letter in range(len(dna) -  len(Str)):
         is_str = True
-        if letter < len(dna) - len(Str):
-            for i in range(len(Str)) :
 
-                if dna[letter + i] != Str[i] :
-                    is_str = False
+        for i in range(len(Str)) :
+            if dna[letter + i] != Str[i] :
+                is_str = False
 
-            if is_str == True:
+        if is_str == True:
+                k = 1
                 count += 1
+                if count > max_count :
+                    max_count = count
+        else :
+            if k >= 1 :
+                k += 1
 
-    print(count)
+        if k > len(Str) :
+            k = 0
+            if is_str == False:
+                count = 0
+
+    return max_count
+
 
 
 
 with open(argv[1] , "r") as data_file :
-    reader = csv.reader(data_file)
-    data = list(reader)
-
+    data = list(csv.reader(data_file))
 
 with open(argv[2] , "r") as dna_file :
     in_dna = dna_file.read()
 
 
-
 DNA_str_info_list = []
+DNA_str_info_list.append("  ")
 
-for  i in range(1 , len(data)) :
-    DNA_str_info_list.append(str_count(in_dna,data[i][0]))
 
-for j in range (1 , len(data[0]))
-    for p in range(len(data))
+for i in range( 1 , len(data[0])) :
+    DNA_str_info_list.append(str_count(in_dna,data[0][i]))
 
-        person_found = True
-        if(data[j][p + 1] != DNA_str_info_list[p]):
-            person_found = False
 
-    if person_found == True :
-        print(data[0][j])
+for j in range (1 , len(data) - 1):
+    found = True
+    for i in range (1 , len(data[0]) - 1):
+
+
+        if data[j][i] != DNA_str_info_list[i]:
+            found = False
+
+    if found == True :
+        print(data[j][0])
         exit(0)
+
+
+
+
+
+
+
+
